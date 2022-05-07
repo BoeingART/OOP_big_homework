8cf1dc943755acb932d01a5290ac28848d9a90e4
BoeingART <boeing.art.a@outlook.com>
2022-05-07 19:45:09 +0800

    错误的

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "mainwindow.h"
-#include "mainwindowclose.h"
 
 #include <QApplication>
 #include <iostream>
@@ -8,12 +7,9 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //cout << 123 << endl;
-    MainWindow gameStarter;
-    gameStarter.setWindowTitle("华容道");
 
-    QObject::connect(gameStarter.about_game, &QPushButton::clicked, &gameStarter, &MainWindow::showAbout);
-    QObject::connect(gameStarter.exit_game, &QPushButton::clicked, &gameStarter, &MainWindow::close);
+    MainWindow gameStarter;
+
     gameStarter.show();
 
     return a.exec();
