0caf9dbde2c8e8c65daa545b952ea591f14ecd59
BoeingART <boeing.art.a@outlook.com>
2022-05-09 16:26:14 +0800

    tmep

--- chessControl/chesscontrol.cpp
+++ chessControl/chesscontrol.cpp
@@ -33,17 +33,16 @@ bool chessControl::chessCorChange(Cor direction){
     if(direction.x == -1 && direction.y == 0)//向左方
     {
         judgeCor = chessCurrentCor[0].x;
-        //if(judgeCor <= 0 || )
     }
     else if(direction.x == 1 & direction.y == 0)//向右方
     {
         judgeCor = chessCurrentCor[size - 1].x;
     }
-    else if(direction.x == 0 && direction.y == -1)//向下方
+    else if(direction.x == 0 && direction.y == -1)//向上方
     {
         judgeCor = chessCurrentCor[0].y;
     }
-    else if(direction.x == 0 && direction.y == 1)//向上方
+    else if(direction.x == 0 && direction.y == 1)//向下方
     {
         judgeCor = chessCurrentCor[size - 1].y;
     }
