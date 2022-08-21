7951cf0e3ec7d7ae9d7bc804300547f569196751
BoeingART <boeing.art.a@outlook.com>
2022-08-21 18:56:57 +0800

    @ 2022年08月21日 18:56:57 Sun

--- include/control/chessconflict.h
+++ include/control/chessconflict.h
@@ -3,6 +3,7 @@
 
 #include "../chessglobal.h"
 
+//坐标类
 struct Cor {
     int x;
     int y;
@@ -16,26 +17,26 @@ struct Cor {
     }
 };
 
+//某处棋盘占据情况类
 class chessConflict {
 public:
-    bool occ;
-    char name;
-
-public:
+    bool occ;   //是否占据
+    char name;  //棋子名字
     chessConflict();
     chessConflict( bool occ_, char name_ );
     void operator=( chessConflict another );
 };
 
+//棋盘局面情况类
 class chessBoard {
 private:
-    chessConflict** chessComplexion;
-    const static int line = 4;
-    const static int row = 5;
+    chessConflict** chessComplexion;  //棋盘局面
+    const static int col = 4;         //棋盘列数col=4
+    const static int row = 5;         //棋盘行数row=5
 
 public:
     chessBoard();
-    void chessBoardChange( const chessConflict board[ 4 ][ 5 ] );
+    void chessBoardChange( const chessConflict board[ col ][ row ] );  /
     chessConflict* const operator[]( const int i );
     void operator=( chessBoard another );
 };
